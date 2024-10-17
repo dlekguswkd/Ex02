3371fe73f262a4cc8130b1cdcfab7f15fba3b76b
dlekguswkd <dlekguswkd@naver.com>
2024-10-17 14:17:39 +0900

    2024-10-17

---
+++ ex01.py
@@ -0,0 +1,143 @@
+# 자료구조  리스트
+print("=== 리스트 기본 ========================")
+
+test_List = [1, 12, 123, 1234]
+
+print(type(test_List))      # type  --> <class 'list'>
+
+print(test_List[0], test_List[3])
+print(test_List[-1])        # -인덱스
+
+print(test_List[1:3])       # 1번방부터 (3-1)번방까지 슬라이싱 문법 가능
+
+print(len(test_List))       # len() 메소드로 갯수 알아내기  
+
+print(test_List*2)          # 한번더 담김 있는게 더 담김 
+print((test_List*2)[5])          # 한번더 담긴거에 5번째방
+print(test_List)            # 원본이 바뀌진않음
+
+print(test_List + [100, 200, 300])          # 리스트에 추가됨
+print(test_List)            # 원본이 바뀌진않음
+
+print(2 in test_List)       # 배열에 있냐? 
+result = 12 in test_List    # 배열에 12가 있으면 true
+print(result)
+
+print("=== 리스트 삭제 ====================")
+# del(test_List[2])
+# del test_List[2]  같은 표현
+print(test_List)        # 원본이 바뀜
+
+
+print("=== 리스트 수정 ====================")
+test_List[0] = "apple"      # 자료형이 다른것도 들어감
+print(test_List)
+
+b_list = ["apple", "banna", 10, 20]
+b_list[2] = b_list[2] + 90      
+print(b_list)
+
+
+print("=== 리스트 수정 ====================")
+test_List = [1, 12, 123, 1234]
+print(test_List[0:2])       # [1, 12]
+test_List[0:2] = [888, 999]
+print(test_List)            # 원본이 바뀜
+
+print(test_List[0:2])       # [888, 999]
+test_List[0:2] = [777]
+print(test_List)            # 자리수가 안맞아도 두개짜리를 하나로 바꿔버림
+print(test_List[0:2])       # [777, 123]
+
+test_List[2:3] = [111]
+print(test_List)
+
+# 추가
+test_List[2:3] = ["aaa", "bbb", "ccc"]      # 갯수가 안맞아도 (모자라도) 그냥 늘어남
+print(test_List)            # [777, 123, 'aaa', 'bbb', 'ccc']
+
+
+
+print("=== 슬라이스를 통한 삭제 ====================")
+a = [1, 12, 123, 1234]
+a[1:2] = []     # 1번째 방 삭제
+print(a)
+
+# 처음부터 끝까지
+a[0:] = []
+print(a)
+
+
+print("=== 슬라이스를 통한 삽입 ====================")
+a = [1, 12, 123, 1234]
+#  [1, "a", 12, 123, 1234] 이렇게 하고싶음
+# a[1:2] = "a"      # 이건 수정개념 12가 a 로 바뀜  a[1:2] -> a[1:1] -> a[1]
+print(a)
+
+a[1:1] = 'a'        # 이건 추가개념 a[1:1] -> a[1:0]
+print(a)
+
+a[5:] = [12345]     # 5번쨰방이 없는데도 추가됨
+print(a)
+
+a[:0] = [-12, -1, 0]    # 맨앞에 추가  [:] 앞뒤 번호가 같으면 추가의 개념
+print(a)
+
+# 슬라이싱 문법 방번호가 같으면 추가 ex) [3:3]
+
+
+print("=== 리스트의 메소드 사용 ====================")
+a = [1, 12, 123, "hello", 3.14, 1000]
+print(a) 
+
+# 추가
+a.append("정우성")      # 맨끝에 추가됨  -> append는 하나만 추가 가능
+print(a)
+
+a.insert(2, "박명수")       # 2번째 방에 추가해줘 나머지는 하나씩 밀림
+print(a)
+
+a.extend([6,7,8])       # 맨끝에 추가됨  -> 여러개 추가하고싶을땐 extend 사용해야함
+print(a)
+
+
+# 삭제
+a.remove(1000)          # 값으로 찾아서 삭제 내가딱 지정해주는게 사라짐
+print(a)
+
+a.pop(6)                # 방번호를 찾아서 삭제 여기에서 6은 6번째방을 의미      del(a[6])
+print(a)
+
+a.pop()                # 방번호(X)로 지우기  -> 방번호를 안적으면 마지막꺼가 사라짐
+print(a)
+
+
+print("=== 리스트의 메소드 사용 ====================")
+b = [1, 123, 1000, 12, 1000]
+print(b)
+
+# 카운트
+print(b.count(1000))        # 리스트에 1000 이 몇개있는지
+
+# 뒤집기
+b.reverse()                 # 원본 리스트의 순서가 뒤집힌다
+print(b)                    # 순서가 반대가됨 -> 원본이 바뀜
+
+# 정렬
+b.sort()                    # 오름차순으로 정렬된다
+print(b)                    # 원본 리스트가 오름차순으로 바뀜
+
+b.sort(reverse=True)        # 원본 리스트가 내림차순으로 정렬된다
+print(b)
+
+# 값으로 index 번호 찾기
+print(b.index(1000))        # 처음 검색되는 방번호 리턴
+
+
+b = [1, 123, 1000, 12, 1000]
+for no in b:
+    print(no)
+
+
+for index,no in enumerate(b):
+    print(index, no)
---
+++ ex03.py
@@ -0,0 +1,84 @@
+# 딕션어리  키-값
+
+print("=== 사전(dict) :딕션어리 ====================")
+
+a = {}          # 딕션어리 생성방법
+
+# 추가
+# "r38" "빅데이터반"
+#  키       값
+a["r38"] = "빅데이터반"
+a["r32"] = "플스택반"
+a["r30"] = "진소영"
+
+print(a) 
+print(type(a))              # <class 'dict'>
+
+# 생성방법
+d = {"basketball": 5, "soccer": 11, "baseball": 9}      # 처음부터 넣고 생성
+d["volleyball"] = 6                                     # 추가
+print(d)
+
+print("=== 딕션어리 값 출력 ====================")
+print(a["r30"])         # 키로 값찾기
+print(d["soccer"])
+# print(d.get("soccer"))      # 위에꺼랑 같은표현 
+
+
+print("=== 딕션어리 값 삭제 -> 키 ====================")
+print(d)
+# del(d["soccer"])      두가지 다 가능
+# d.pop("soccer")
+print(d)
+
+print("=== 딕션어리 값 삭제 -> 전체 ====================")
+d = {"basketball": 5, "soccer": 11, "baseball": 9}   
+print(d)
+
+# 다 지우는법 두가지 다 가능
+# d ={}
+d.clear()
+print(d)
+
+
+print("=== 딕션어리 찾기(검색) ====================")
+d = {"basketball": 5, "soccer": 11, "baseball": 9}   
+print(d)
+print(len(d))           # 갯수
+print("soccer" in d)        # 키 값중에 d에 soccer  라는아이가 있니?
+print("soccer" not in d)     # 키 값중에 d에 soccer  라는아이가 없니?
+
+
+print("=== 딕션어리의 키들을 리스트로 반환 ====================") # 키들의 이름을 모를때
+d = {"basketball": 5, "soccer": 11, "baseball": 9}   
+print(d)
+
+# 내가정한이름 = 메소드에 있는 이름keys
+keys = d.keys()
+print(keys, type(keys))
+
+
+# 키를 리스트로 갖고있어야함
+#   하나담을 내가정한이름  in 전체이름
+for key in keys:
+    print(key)              # 키뿌리기
+    print(d[key])           # 값뿌리기
+    #print(f"{key} ->  {d[key]}")        # 키와, 값 뿌리기
+
+
+print("=== 딕션어리의 값들을 리스트로 반환 ====================")
+
+# 내가정한이름  = 메소드에 있는 값 values
+values = d.values()         # 값들만 뽑기
+print(values)       # 순서는 마음대로 나옴
+print(type(values))
+
+for value in values:
+    print(value)
+
+
+
+# 딕션어리는 방번호의 개념이 없음 -> 이걸로 리스트처럼 
+play_list = d.items()
+print(play_list, type(play_list))
+
